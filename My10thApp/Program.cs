60d0088bc20a6b749d2a0dc71a3edf0d5de07eb0
SingaraveluSStackRad <SingaraveluSStackRad@STACKRADIUSLT01>
2023-04-03 23:14:09 +0530

    Add project files.

---
+++ My10thApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace My10thApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ My9thApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace My9thApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
